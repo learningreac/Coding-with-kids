d9e74e32cf4d1fab83e3e98c4f56cd027213d27c
learningreac <learningreact2020@gmail.com>
2022-12-01 22:03:02 -0800

    show Collision msg and stop the game

--- Iceberg_Hit/main.js
+++ Iceberg_Hit/main.js
@@ -3,8 +3,9 @@
 const iceContainer = document.querySelector("#icebergs");
 let screenHeight = document.body.clientHeight
 let screenWidth = document.body.clientWidth;
-
 const col = 12, row = 8;
+let intervalID;
+let isCollision = false;
 // if transfer into px, it will be easier to attect the colosion by x and y
 // now will generate the size of the iceberg by screen width and height
 // it will be 8 * 12 grids, each iceberg take one grids;
@@ -12,14 +13,30 @@ const col = 12, row = 8;
 let icebergSize = screenHeight < screenWidth ? screenHeight / row : screenHeight / col;
 icebergSize = Math.floor(icebergSize); // px
 
+//  Dom Elements
+const countdownView = document.querySelector("#countdown-view");
+const collisionView = document.querySelector("#collision-view");
+
+console.log('views', countdownView, collisionView)
+
 const startX = screenWidth * .6;
 const restartX = -icebergSize * 10;
 console.log('windowSize', screenWidth, screenHeight, icebergSize);
 
-// boat keyboard events
+// helper functions
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+}
+
+const getRandomArbitrary = (min, max) => {
+  return Math.random() * (max - min) + min;
+}
+
+
+// start boat keyboard events
 const boat = document.querySelector("#boat")
 document.addEventListener('keydown', handleKeydown);
-document.addEventListener('keyup', handleKeyUp);
+// document.addEventListener('keyup', handleKeyUp);
 
 const setBoat = () => {
   boat.style.width = icebergSize + "px";
@@ -32,38 +49,69 @@ const setBoat = () => {
 setBoat();
 
 function handleKeydown(event) {
-  //console.log('keydown', boat.style)
+  console.log('keydown', boat.style.left)
 
   if (boat.style.top === "") {
     boat.style.top = "50%"
   }
 
   let curY = parseInt(boat.style.top);
+  let curX = parseInt(boat.style.left);
+
   if (event.keyCode === 40) { // Arrow down
     curY += 1;
     // console.log('down', event.code)
-    boat.style.top = curY + "%"
+    boat.style.top = curY + "%";
   } else if (event.keyCode === 38) { // Arrow up
     curY -= 1;
     // console.log('up', curY)
-    boat.style.top = curY + "%"
+    boat.style.top = curY + "%";
+  } else if (event.keyCode === 37) { // left Arrow
+    curX -= 1;
+    boat.style.left = curX + "%";
+  } else if (event.keyCode === 39) { // right Arrow
+    curX += 1;
+    boat.style.left = curX + "%";
   }
 }
 
-function handleKeyUp(event) {
-  console.log('keyup', event.code)
+// function handleKeyUp(event) {
+//   console.log('keyup', event.code)
+// }
+
+
+// start btn click events
+const startBtn = document.querySelector("#start-btn");
+startBtn.addEventListener('click', handleStartBtnClick);
+
+
+function handleStartBtnClick(event) {
+  // startBtn.classList.add('hidden');
+  // const countdownText = document.querySelector("#countdown-text")
+  // // handle counting down
+  // countdownText.classList.remove('hidden');
+  // let i = 3;
+  // while (i > 0) {
+  //   setTimeout(() => { countdownText.textContent = i; i-- }, 1000)
+  //   // handleCountdown
+  // }
+
+  countdownView.classList.add('hidden');
+  StartGame();
 }
 
-const getRandomInt = (max) => {
-  return Math.floor(Math.random() * max);
+
+const StartGame = () => {
+  isCollision = false;
+  animateAllIcebers();
+  intervalID = setInterval(animateAllIcebers, 30);
+  console.log('collision', isCollision)
 }
 
 
 
-const getRandomArbitrary = (min, max) => {
-  return Math.random() * (max - min) + min;
-}
 
+// start iceberg related functions
 
 const generateIceberg = () => {
   const iceberg = document.createElement('div');
@@ -79,21 +127,15 @@ const generateIceberg = () => {
 const animateOneIceberg = (iceberg) => {
   let curLeft = parseInt(iceberg.style.left);
 
-  // console.log('js worked', 'curleft', curLeft)
-
   if (curLeft > screenWidth) {
     curLeft = restartX;
-    // top not change 
   }
 
   curLeft += 2;
   iceberg.style.left = curLeft + "px";
-  // console.log('new left', iceberg.style.left)
 }
 
 const generateOneColIcebers = (containerDiv, colId, total, left) => {
-  // now total is equal row number, 
-  // control if draw iceberg on that grid by random (0,1);
   let top = getRandomArbitrary(-3, 10);
 
   for (let i = 0; i < total; i++) {
@@ -128,54 +170,56 @@ const generateMultiCols = (cols, row) => {
 };
 
 const animateAllIcebers = () => {
-  // console.log('animation')
+
+  console.log('interval', intervalID)
   const cols = iceContainer.childNodes;
 
   for (const node of cols) {
     const divs = node.childNodes;
-    // const curColLeft = parseInt(node.style.left);
-    // console.log('check', curColLeft);
     for (const iceberg of divs) {
       animateOneIceberg(iceberg);
-      isCollision(iceberg, boat);
-      // iceberg.style.backgroundColor = "yellow"
+      if(checkCollision(iceberg, boat)) {
+        isCollision = true;
+        afterCollision();
+        return;
+      };
     }
   }
 }
 
-const isCollision = (iceberg, boat) => {
+const afterCollision = () => {
+  console.log('after', isCollision)
+  if(isCollision) {
+    window.clearInterval(intervalID);
+    collisionView.classList.remove('hidden');
+
+  }
+};
+
+const checkCollision = (iceberg, boat) => {
   //  if the right of the iceberg is bigger than the left of the boat, then check top
   const left = parseInt(iceberg.style.left);
   const right = left + icebergSize;
   const top = parseInt(iceberg.style.top);
   const bottom = top + icebergSize;
 
-  const boatPosLeft = parseInt(boat.style.left)/100 * screenWidth;
-  const boatPosTop = parseInt(boat.style.top)/100 * screenHeight;
+  const boatPosLeft = parseInt(boat.style.left) / 100 * screenWidth;
+  const boatPosTop = parseInt(boat.style.top) / 100 * screenHeight;
   const boatPosBottom = boatPosTop + icebergSize / 2;
 
-  // console.log('boat', boatPosLeft, boatPosTop, boatPosBottom);
-  // console.log('COLLISION', right, top, bottom)
-  let result = false
-
+  let result = false;
   // check collision
   if (right + 1 >= boatPosLeft) {
     // check top
     if (boatPosTop >= top && boatPosTop <= bottom) {
-      console.log('collision')
       result = true; // collision
     } else if (boatPosBottom >= top && boatPosBottom <= bottom) {
       result = true;
-      console.log('collision')
     }
   }
-
-  
 
   return result;
 
 }
 screenWidth > screenHeight ? generateMultiCols(col, row) : generateMultiCols(row, col)
 
-// animateAllIcebers();
-// setInterval(animateAllIcebers, 30);
